e2da0623a4d5a17a4b405f4ac616f759289aa0a3
MuhammadNomanShoukat <43296192+MuhammadNomanShoukat@users.noreply.github.com>
2024-08-01 18:07:15 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,7 +44,48 @@ const coerceRequestBody = (body) => ({
 const joinGeoRegions = (geoRegions) => toStringArray(geoRegions)?.join(',')
 
 app.post('/', (req, res) => {
-  res.end("Hello");
+  const requestBody = coerceRequestBody(req.body)
+  const validationErrors = validateRequest(requestBody, validator)
+
+  if (validationErrors.length > 0) {
+    return res.status(400).json({ errors: validationErrors })
+  }
+
+  const {
+    role,
+    sessionName,
+    expirationSeconds,
+    userIdentity,
+    sessionKey,
+    geoRegions,
+    cloudRecordingOption,
+    cloudRecordingElection,
+    audioCompatibleMode
+  } = requestBody
+
+  const iat = Math.floor(Date.now() / 1000)
+  const exp = expirationSeconds ? iat + expirationSeconds : iat + 60 * 60 * 2
+  const oHeader = { alg: 'HS256', typ: 'JWT' }
+
+  const oPayload = {
+    app_key: process.env.ZOOM_VIDEO_SDK_KEY,
+    role_type: role,
+    tpc: sessionName,
+    version: 1,
+    iat,
+    exp,
+    user_identity: userIdentity,
+    session_key: sessionKey,
+    geo_regions: joinGeoRegions(geoRegions),
+    cloud_recording_option: cloudRecordingOption,
+    cloud_recording_election: cloudRecordingElection,
+    audio_compatible_mode: audioCompatibleMode
+  }
+
+  const sHeader = JSON.stringify(oHeader)
+  const sPayload = JSON.stringify(oPayload)
+  const sdkJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, process.env.ZOOM_VIDEO_SDK_SECRET)
+  return res.json({ signature: sdkJWT })
 })
 
 app.listen(port, () => console.log(`Zoom Video SDK Auth Endpoint Sample Node.js, listening on port ${port}!`))
